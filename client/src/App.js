732bb66c0406c5b63d3d1845e827cd7561c97391
IamSamyak <samyakburnure.scoe.it@gmail.com>
2024-06-16 17:34:18 +0530

    Removing padding from header and Adding footer

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,20 @@ import Form from './componets/Form';
 function App() {
   return (
     <div className="App">
-      <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
-      <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounder">Expense Tracker</h1>
+      <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounded">Expense Tracker</h1>
+    <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
       <div className="grid md:grid-cols-2 gap-4">
         {/* chart */}
         <Graph></Graph>
         {/* form */}
         <Form></Form>
       </div>
-      </div>
     </div>
+      <footer className="mt-10 bg-slate-800 text-white py-4 rounded">
+        &copy; Akash Kamat 2024
+      </footer>
+  </div>
+  
   );
 }
 
